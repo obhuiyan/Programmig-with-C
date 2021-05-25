2f9f6d65fed3be239a1980a6c13ba40d236f2509
obhuiyan <84793254+obhuiyan@users.noreply.github.com>
2021-05-25 11:38:15 -0400

    Create 05-25-2021.c

---
+++ 05-25-2021.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,result;
+    A=200;
+    B=52;
+    result=A+B;
+    printf("the addition of %d and %d is:%d\n",A,B,result);
+    result=A-B;
+    printf("the substraction of %d and %d is: %d\n",A,B,result);
+    result=A+B+A-B;
+    printf("the reslt of A+B+A-B is: %d\n",result);
+    return 0;
+}
+    
+ 
